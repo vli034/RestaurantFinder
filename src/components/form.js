2874f0158426e493fcbeb9ffa30e056e8f0ca8db
vanessali <li.vanessaa@gmail.com>
2019-07-20 13:57:17 -0400

    react web page funtionality completed

--- src/components/form.js
+++ src/components/form.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 // import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
 
 var apiKey ='9399d401d83ef3ae9c95f9c9907d8376';
 
@@ -8,97 +9,107 @@ var apiKey ='9399d401d83ef3ae9c95f9c9907d8376';
 class MyForm extends React.Component{
     constructor(){
         super();
-        this.state={
+        this.state={                                           //initialized these mfs to empty state 
             userInput:'',
             cuisineArray:[],
-            restoArray:[],      //initialized these mfs to empty state 
-            isFetching: false   
+            restoArray:[],      
         }
     }
-    handleChange=(event)=>{
-        //would i create a conditional statement here to check if the user input === a cuisine name in cusine array?
-       
-        //take the user input and loop it through the cuisineArray to check if it matches anything in the array 
-        // if it matches then  return the cuisine_id
-        // pass the cuisine_id as the parameter in fetchRestaurantData() as q: paramater
-
-
-        this.setState({//re render dom when user access keeebs
-            userInput: event.target.value           // target is a method in event that allows us to target the event that occurs  in the input text and we grab the value
-
+    handleChange=(event)=>{                                  // Has to retreive the user input value that we get in our handleChange method. This gets the string value of whatever the user has inputted 
+       this.setState({                                       //re render dom when user access keeebs
+            userInput: event.target.value           
         })
-
-        console.log(event.target.value)
-        
-
     }
+
+    // This is a function that gets called when the user clicks submit
+    handleOnSubmit() {
+        const onlyCuisineWeCareAbout = this.state.cuisineArray.filter((cuisine) => {         //grab the current state of user input -- that we get from handle change.
+            return cuisine.cuisine_name === this.state.userInput                            //Filtered through the cuisine array that holds objects with only cuisine ID and name and return the cuisine name that matches the string inputted from user
+        })
+    //    console.log(onlyCuisineWeCareAbout)                                               //Then CuisneWecareAbout returns an array(filter returns array) with ONE object from cuisineArray that matches user input 
+
+        this.fetchRestaurantData(onlyCuisineWeCareAbout[0].cuisine_id);                     //Called the second api to access the first and only element in that array, which is an object and the property we wa t
+                                                                                            // PASS IT the cuisine_id value that we are able to get from the  object attributes-- cuisine_id
+        
+        //OnClick - clear user input field 
+        this.setState({userInput: ''})
+    }
+
+    //Retrieve cuisine data via zomato api
     async fetchCuisineData(){
         try{
             const cuisineUrl = 'https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/cuisines'
             const types = await axios.get(cuisineUrl,{
                 params:{
-                    city_id: 3
+                    city_id: 7
                 },
                 headers:{
                     'user-key':apiKey
                 }
             })
-            this.setState({
-                cuisineArray: types.data.cuisines       //array has been populated with cuisine objects that hold cuisnine_id and cuisine_name
+            
+            const cus_arr = types.data.cuisines //grab our formatted array from our API in types and hold that array in a constant 
+            const cuisineName = cus_arr.map((cus)=>{ // map it out
+                return cus.cuisine
             })
-            // console.log(types.data.cuisines);
+
+            this.setState({
+                cuisineArray: cuisineName    //setting the state of initialized array witth cus_arr that is one level deeper giving us cuisine_name/ & id
+            })
+
+            // console.log(test)
+            // console.log(this.state.cuisineArray);
 
         }catch(error){console.log(error.message)}
     }
 
-    async fetchRestaurantData(){
+    //retrieve all the restaruants with associating cuisine 
+    async fetchRestaurantData(cuisineId){ // takes a parameter bc this api endpoint needd a query param --set a parama called cuisineId
         try{
             const url = 'https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/search'
             const restaurantOptions = await axios.get(url,{
                 params:{
-                    q:'pizza'
+                    cuisines: cuisineId
                 },
                 headers:{
                     'user-key': apiKey
                 }
             })
             this.setState({
-                restoArray:restaurantOptions.data.restaurants          //populated array with resto objects -- therefore now we have access to name property 
+                restoArray:restaurantOptions.data.restaurants          // populat the  empty array with resto objects that match the cuisine from user input (search end point knows which resto with goes with which cuisine via the cuisine id )
             })
-            console.log(restaurantOptions.data.restaurants);       //restaruant(object).data(object).restaurants(array)-- array of objects that holds different restos for given query
+           // console.log(restaurantOptions.data.restaurants);       //restaruant(object).data(object).restaurants(array)-- array of objects that holds different restos for given query
+            console.log(this.state.restoArray)
 
-
+            /*const test =this.state.restoArray.map((res)=>{               //by inserting a fixed query value, const restaurOtions give us an array of options that match the cuisine vakue and 
+                return console.log(res.restaurant.name);                 //by making over all the objects we retrieve a name  
+            })*/
         }catch(error){console.log(error.message)}
     }
-    test(){ // cusine array had an array of 'types of cuisine objects'
-        const cuisineName = this.state.cuisineArray.map((cus)=>{
-            return console.log(cus.cuisine)               /*cus.cuisine populates my array with objects and cus.cuisine.cuisine_name gives me just name*/          //this object was nested as fuck, fuck you zomato API
-        })
 
-        return(          
-            <section>
-                <p>this array has been populated</p>
-                {/* <div> {cuisineName}</div> */}
-            </section>
-        )         
-    }
-
+    // this function is called and render always checking if the array has been filled 
     renderRestaurantOptions(){
-        const restaurantName = this.state.restoArray.map((res)=>{
-            return res.restaurant.name
+        const restaurantName = this.state.restoArray.map((res)=>{    //must return jsx to show on second return      
+            return (
+                <div key={res.restaurant.id}>
+                    <h2>{res.restaurant.name}</h2>
+                    <p>Average cost for 2 persons: ${res.restaurant.average_cost_for_two}</p>
+                    <img alt='' src={res.restaurant.featured_image}/>
+     
+                </div>
+            )          
         })
         return(
-            <section>
-                <p>{restaurantName} </p> 
-            </section>
+            <div>
+                <div>{restaurantName} </div>   
+            </div>
 
         )
     }
 
-   
+   //Calling our functions that retrieve API data 
     componentDidMount(){
-        // this.fetchCuisineData();
-        this.fetchRestaurantData();
+        this.fetchCuisineData();
     }
     render(){
         return(
@@ -106,12 +117,15 @@ class MyForm extends React.Component{
                 <label>
                     Enter type of cuisine you want to eat:
                     <input type='text' value={this.state.userInput} onChange={this.handleChange}/>
+                    <Button variant='contained' color='primary' onClick={()=>this.handleOnSubmit()}>submit</Button>
                 </label>
                 <h2>List of Restaurant Options</h2>
                 { this.state.restoArray.length >0 ? this.renderRestaurantOptions():<p>the resto array is empty</p> }
                
-                <h2> list of cuisines</h2>
-                { this.state.cuisineArray.length >0 ? this.test():<p>the cuisine array is empty</p> }
+                {/* <h2> list of cuisines</h2> */}
+                {/* { this.state.cuisineArray.length >0 ? this.renderCuisine():<p>the cuisine array is empty</p> } */}
+                {/* {this.handleOnSubmit()} */}
+                {/* {this.renderRestaurantOptions()} */}
 
             </main>
         )
